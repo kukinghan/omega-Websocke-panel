d4703392b03e59f00728f465441adcd34b803c43
kukinghan <kukinghan@163.com>
2022-07-26 03:07:02 +0800

    1

--- js/WebSocket.js
+++ js/WebSocket.js
@@ -1,23 +1,23 @@
-var msg;//传参消息
-var msgID = 0;//请求序号
-var msgDiv = document.getElementById("omg-msgDiv");//右侧返回列表
+var msg; //传参消息
+var msgID = 0; //请求序号
+var msgDiv = document.getElementById("omg-msgDiv"); //右侧返回列表
 window.onload = setSocket();
-var socket;//WebSocket，待优化
-var omgData;//omg返回内容
+var socket; //WebSocket，待优化
+var omgData; //omg返回内容
 
 /*
-*	本js负责WebSocket通信
-*	处理返回内容
-*/
+ *	本js负责WebSocket通信
+ *	处理返回内容
+ */
 
 // WebSocket连接
-function setSocket(){
+function setSocket() {
 	let classs = document.getElementsByClassName("omgLink");
 	for (let i = 0; i < classs.length; i++) {
 		classs[i].innerHTML = '<span style="color: orange;">正在尝试与omg通信❥~~~</span>'
 	}
-	
-	socket = new WebSocket('ws://'+String(document.getElementById("socket").value)+'/omega_side ')
+
+	socket = new WebSocket('ws://' + String(document.getElementById("socket").value) + '/omega_side ')
 
 	socket.addEventListener('open', function() {
 		for (let i = 0; i < classs.length; i++) {
@@ -29,17 +29,17 @@ function setSocket(){
 		console.log(e.data);
 		// 处理数据
 		let jsonMsg = JSON.parse(e.data);
-		omgData = jsonMsg;//保存结果
-		msgDiv.innerHTML += "请求"+msgID+"：";
-		
+		omgData = jsonMsg; //保存结果
+		msgDiv.innerHTML += "请求" + msgID + "：";
+
 		if (jsonMsg.violate == false) {
 			msgDiv.innerHTML += "成功";
-		} else{
+		} else {
 			msgDiv.innerHTML += "失败";
 		}
 
 		msgDiv.innerHTML += "<br />";
-		
+
 	})
 	socket.addEventListener('close', function() {
 		for (let i = 0; i < classs.length; i++) {
@@ -49,10 +49,10 @@ function setSocket(){
 }
 
 // 重连WebSocket//留个函数等扩展
-function socketClose(){
-	try{
+function socketClose() {
+	try {
 		socket.close();
-	}catch(e){
+	} catch (e) {
 		console.log("无连接")
 	}
 	// 返回main
@@ -62,17 +62,16 @@ function socketClose(){
 }
 
 // 通信
-function shellMsg(msg){
+function shellMsg(msg) {
 	socket.send(msg);
 }
 //Websocket
-function WebsocketMsg(msgObj){
-	msg = `{"client":`+(msgID+=1)+`,"function":"send_ws_cmd","args":{"cmd":"`+msgObj+`"}}`
+function WebsocketMsg(msgObj) {
+	msg = `{"client":` + (msgID += 1) + `,"function":"send_ws_cmd","args":{"cmd":"` + msgObj + `"}}`
 	socket.send(msg)
 }
 //Player
-function PlayerMsg(msgObj){
-	msg = `{"client":`+(msgID+=1)+`,"function":"send_player_cmd","args":{"cmd":"`+msgObj+`"}}`
+function PlayerMsg(msgObj) {
+	msg = `{"client":` + (msgID += 1) + `,"function":"send_player_cmd","args":{"cmd":"` + msgObj + `"}}`
 	socket.send(msg)
 }
-
--- js/shell.js
+++ js/shell.js
@@ -1,148 +1,164 @@
-let timeVal;//设定时间值
-var playerListArray = {};//临时储存的玩家列表
-
-onload = function(){
+let timeVal; //设定时间值
+var playerListArray = {}; //临时储存的玩家列表
+onload = function() {
 	mianSwitch();
 };
 
+
 /*
-*
-*	这里是界面逻辑
-*	
-*/
+ *
+ *	这里是界面逻辑
+ *	
+ */
 // 页面切换（排它显示）
-	// 隐藏
-	function showNone(){
-		let obj = document.getElementsByClassName("show");
-		for (var i = 0; i < obj.length; i++) {
-			obj[i].style = "display: none;"
-		}
-	}
-	// 显示
-	function showSwitch(obj){
-		document.getElementById(obj).style = "display: inline";
-	}
-	function mianSwitch(){
-		showNone();
-		showSwitch("list-mian");
-	}
-	function commandSwitch(){
-		showNone();
-		showSwitch("list-command");
-	}
-	function worldSwitch(){
-		showNone();
-		showSwitch("list-world");
-	}
-	function playerSwitch(){
-		showNone();
-		showSwitch("list-player");
-	}
-	function inputSwitch(){
-		showNone();
-		showSwitch("list-input");
-	}
-	function jsonsetSwitch(){
-		showNone();
-		showSwitch("list-jsonset");
-	}
-	function inquireSwitch(){
-		showNone();
-		showSwitch("list-slelect");
-	}
+// 隐藏
+function showNone() {
+	let obj = document.getElementsByClassName("show");
+	for (var i = 0; i < obj.length; i++) {
+		obj[i].style = "display: none;"
+	};
+};
+
+// 显示
+function showSwitch(obj) {
+	document.getElementById(obj).style = "display: inline";
+};
+
+function mianSwitch() {
+	showNone();
+	showSwitch("list-mian");
+};
+
+function commandSwitch() {
+	showNone();
+	showSwitch("list-command");
+};
+
+function worldSwitch() {
+	showNone();
+	showSwitch("list-world");
+};
+
+function playerSwitch() {
+	showNone();
+	showSwitch("list-player");
+};
+
+function inputSwitch() {
+	showNone();
+	showSwitch("list-input");
+};
+
+function jsonsetSwitch() {
+	showNone();
+	showSwitch("list-jsonset");
+};
+
+function inquireSwitch() {
+	showNone();
+	showSwitch("list-slelect");
+};
 
 
 /*命令执行页
-*
-*	这里是命令执行页功能
-*
-*/
+ *
+ *	这里是命令执行页功能
+ *
+ */
 // 发送请求
-	// jsonMsg
-	function msgShell(){
-		msg = document.getElementById("msg-json").value;
-		shellMsg(msg);
-	};
-	//WebsocketMsg
-	function msgWebsocket(){
-		let msgObj = document.getElementById("msg-Websocket").value;
-		WebsocketMsg(msgObj)
-	};
-	//PlayerMsg
-	function msgPlayer(){
-		let msgObj = document.getElementById("msg-Player").value;
-		PlayerMsg(msgObj)
-	};
+// jsonMsg
+function msgShell() {
+	msg = document.getElementById("msg-json").value;
+	shellMsg(msg);
+};
+//WebsocketMsg
+function msgWebsocket() {
+	let msgObj = document.getElementById("msg-Websocket").value;
+	WebsocketMsg(msgObj)
+};
+//PlayerMsg
+function msgPlayer() {
+	let msgObj = document.getElementById("msg-Player").value;
+	PlayerMsg(msgObj)
+};
 
 
 
 /*世界控制页
-*
-*	这里是界面功能
-*
-*/
+ *
+ *	这里是界面功能
+ *
+ */
 
 // 时间显示
-	function numValueGain(){
-		let valNum = document.getElementById("timeRangeValue");
-		timeVal = document.getElementById("timeRange").value;	
-		valNum.innerHTML = timeVal;
-	};
-	function msgTimeSet(){
-		WebsocketMsg("/time set "+timeVal);
-	};
-// 天气控制
-	function weatherClear(){
-		WebsocketMsg("/weather clear");//晴
-	};
-	function weatherRain(){
-		WebsocketMsg("/weather rain");//雨
-	};
-	function weatherThunder(){
-		WebsocketMsg("/weather thunder");//暴雨
-	};
+function numValueGain() {
+	let valNum = document.getElementById("timeRangeValue");
+	timeVal = document.getElementById("timeRange").value;
+	valNum.innerHTML = timeVal;
+};
 
+function msgTimeSet() {
+	WebsocketMsg("/time set " + timeVal);
+};
+
+// 天气控制
+function weatherClear() {
+	WebsocketMsg("/weather clear"); //晴
+};
+
+function weatherRain() {
+	WebsocketMsg("/weather rain"); //雨
+};
+
+function weatherThunder() {
+	WebsocketMsg("/weather thunder"); //暴雨
+};
+
+//实体清理
+function killItems() {
+	WebsocketMsg("/kill @e[type=item]");
+};
 /*玩家控制页
-*
-*	这里是界面功能
-*
-*/
+ *
+ *	这里是界面功能
+ *
+ */
 // 获取玩家列表
-	function playerList(){
-		msg = `{"client":`+(msgID+=1)+`,"function":"get_players_list","args":{}}`
-		shellMsg(msg);
-		setTimeout(function() {
-			for (let i = 0; i < omgData.data.length; i++) {
-				document.getElementById("player-list").innerHTML += "玩家："+omgData.data[i].name+"<br />";
-				let playerArray = {};
-				playerArray.push(omgData.data[i].name);
-				console.log(playerArray);
-			}
-		}, 10);
-		
-	};
+function playerList() {
+	msg = `{"client":` + (msgID += 1) + `,"function":"get_players_list","args":{}}`
+	shellMsg(msg);
+	setTimeout(function() {
+		for (let i = 0; i < omgData.data.length; i++) {
+			document.getElementById("player-list").innerHTML += "玩家：" + omgData.data[i].name + "<br />";
+			let playerArray = {};
+			playerArray.push(omgData.data[i].name);
+			console.log(playerArray);
+		}
+	}, 10);
+
+};
 
 
 /*建筑导入页
-*
-*	这里是界面功能
-*
-*/
+ *
+ *	这里是界面功能
+ *
+ */
 
 
 
 /*配置编辑页
-*
-*	这里是界面功能
-*
-*/
+ *
+ *	这里是界面功能
+ *
+ */
 
 
 
 /*数据查询页
-*
-*	这里是界面功能
-*
-*/
-	
+ *
+ *	这里是界面功能
+ *
+ */
+
 // {"client":1,"violate":false,"data":{"result":{"CommandOrigin":{"Origin":5,"UUID":"342e271e-09b1-11ed-b269-525400b670a0","RequestID":"96045347-a6a3-4114-94c0-1bc4cc561694","PlayerUniqueID":0},"OutputType":4,"SuccessCount":1,"OutputMessages":[],"DataSet":"{\n \"message\" : \"hi\",\n \"statusCode\" : 0\n}\n"}}}
